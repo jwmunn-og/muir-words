6c210a61a72ea2775fad38e796cb61f5000b2e4b
Justin Munn <contact@justinmunn.co>
2017-01-22 04:29:26 +0000

    Changed homepage layout, added background images, changed webfonts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def randomized_background_image
+    images = ["assets/yosemite-falls.jpg", "assets/wind-river.jpg", "assets/mt-sneffels.jpg" ]
+    images[rand(images.size)]
+  end
 end
